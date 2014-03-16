340a1626c75d92391f13652b0a10047fd41c7e20
Tiago Shibata <tiago.shibata@gmail.com>
2014-03-15 22:40:52 -0300

    Aplicativo inicial ADT
    Activity com texto e caixa de texto

---
+++ src/app/caronacomunitaria/br/Main.java
@@ -0,0 +1,14 @@
+package app.caronacomunitaria.br;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import app.caronacomunitaria.br.R;
+
+public class Main extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
