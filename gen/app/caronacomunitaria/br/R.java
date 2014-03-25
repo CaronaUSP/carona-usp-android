9f0640737b60a3ed6cf0a74042fadf70f640603e
Tiago Shibata <tiago.shibata@gmail.com>
2014-03-25 19:21:48 -0300

    Interface atualizada
    Pede-se e-mail USP (mais fácil de controlar e confirmar do que número)
    Botão de cadastro

--- gen/app/caronacomunitaria/br/R.java
+++ gen/app/caronacomunitaria/br/R.java
@@ -14,16 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f060003;
-        public static final int button2=0x7f060002;
+        public static final int cadastro=0x7f060004;
+        public static final int dar_carona=0x7f060002;
         public static final int login=0x7f060000;
         public static final int password=0x7f060001;
+        public static final int receber_carona=0x7f060003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int cadastro=0x7f040006;
         public static final int dar_carona=0x7f040004;
         public static final int login=0x7f040001;
         public static final int login_field=0x7f040002;
